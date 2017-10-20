24c52bba2428e5021671110031eeac4a65c10b24
wblair <wblair@northernsafety.com>
2017-10-20 16:26:55 -0400

    made initial angular site

---
+++ javascript/app.js
@@ -0,0 +1,22 @@
+/* Define the application/module */
+
+/* No anonymous functions! */ 
+(function(){
+
+  /* Grab the module directive */
+  var app = angular.module('githubSite', ['ngRoute']);
+
+  /* map directory */
+  app.config(function($routeProvider)
+  {
+    $routeProvider
+      .when("/main", {
+        templateUrl: "main.html",
+        controller: "MainCtrl"
+      })
+      .otherwise({
+        redirectTo: "/main"
+      })
+  });
+
+}());
