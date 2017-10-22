77d2c4f58bbb66a46af6ff29df39d804a61fc725
williamblair <blairw@cs.sunyit.edu>
2017-10-22 19:54:28 -0400

    switch to just bootstrap

---
+++ javascript/footer.js
@@ -0,0 +1,9 @@
+var footer = document.getElementById("pageFooter");
+
+footer.innerHTML = 
+`
+  <div class="container">
+  <span class="text-muted" id="lastModified">
+	</span>
+  </div>
+`;
---
+++ javascript/navbar.js
@@ -0,0 +1,34 @@
+/* Write what we want to the contents of the nav bar */
+
+var nav = document.getElementById("pageNav");
+
+nav.innerHTML = 
+`
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand" href="#">williamblair.github.io</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+		<!-- for dropdown -->
+        <!--<li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Page 1-1</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Page 1-3</a></li>
+          </ul>
+        </li>-->
+		<li><a href="WebGL/webgltest.html">WebGL</a></li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+      </ul>
+    </div>
+  </div>
+`;
