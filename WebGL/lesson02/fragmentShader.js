4d0c291276de28502aba8f916a4ec40b736ed433
wblair <wblair@northernsafety.com>
2017-10-26 14:33:33 -0400

    testing removing comments

--- WebGL/lesson02/fragmentShader.js
+++ WebGL/lesson02/fragmentShader.js
@@ -6,7 +6,6 @@ var fragmentShaderText =
 `
 precision mediump float;
 
-// sent from the vertex shader
 varying vec4 vColor;
 
 void main(void) {
--- WebGL/lesson02/vertexShader.js
+++ WebGL/lesson02/vertexShader.js
@@ -5,15 +5,15 @@
 var vertexShaderText = 
 `
 attribute vec3 aVertexPosition;
-attribute vec4 aVertexColor; // get the color vec from the program
+attribute vec4 aVertexColor;
 
 uniform mat4 uMVMatrix;
 uniform mat4 uPMatrix;
 
-varying vec4 vColor; // the vec to send to the fragment shader
+varying vec4 vColor;
 
 void main(void) {
     gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);
-    vColor = aVertexColor; // map the code color vec to our sending color vec
+    vColor = aVertexColor;
 }
 `;
