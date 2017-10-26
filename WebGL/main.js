b5b225f70e44f88d4deb989b29e4079b74efd416
wblair <wblair@northernsafety.com>
2017-10-26 12:13:04 -0400

    testing moving extra/misc to main

--- WebGL/main.js
+++ WebGL/main.js
@@ -1,6 +1,27 @@
 /* kind of the main() function, called
  * when the document body is loaded */
 
+/* global variables */
+/* the OpenGL context */
+var gl;
+  
+/* shader program id */
+var shaderId;
+  
+/* vertex/array buffers for the square and triangle */
+var triangleVertexPositionBuffer;
+var squareVertexPositionBuffer;
+
+var mvMatrix = mat4.create(); // move/translate matrix
+var pMatrix = mat4.create();  // projection matrix
+
+/* utility function - makes the above 2 matrices active */
+/* tell opengl we're using these as our translation and projection matrices */
+var setMatrixUniforms = function() {
+  gl.uniformMatrix4fv(shaderId.pMatrixUniform, false, pMatrix);
+  gl.uniformMatrix4fv(shaderId.mvMatrixUniform, false, mvMatrix);
+};
+
 /* basically main() */
 var webGLStart = function() {
   /* Get the canvas */
