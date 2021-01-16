2a2b51461070d6255e724f1422f30e1bc7e02df1
williamblair <blairw@cs.sunyit.edu>
2021-01-16 12:28:57 -0500

    updated media, started tetris

---
+++ templates/tetris/renderer.js
@@ -0,0 +1,43 @@
+var renderer = {
+
+	width: 0,
+	height: 0,
+	canvas: undefined,
+	ctx: undefined,
+	
+	Init: function( width, height )
+	{
+		this.width  = width;
+		this.height = height;
+		
+		this.canvas = document.getElementById( "mycanvas" );
+		this.ctx = this.canvas.getContext( "2d" );
+	},
+	
+	Clear: function()
+	{
+		this.ctx.fillStyle = "#000000";
+		this.ctx.fillRect( 0, 0, this.width, this.height );
+	},
+	
+	DrawCircle( x, y, radius, color )
+	{
+		this.ctx.fillStyle = color;
+		this.ctx.beginPath();
+		this.ctx.arc( x, y, radius, 0, 2 * Math.PI );
+		this.ctx.stroke();
+	},
+	
+	DrawSquare( x, y, size, color )
+	{
+		this.ctx.fillStyle = color;
+		this.ctx.fillRect( x, y, size, size );
+	},
+    
+    DrawText( x, y, font, color, message )
+    {
+        this.ctx.font = font;
+        this.ctx.fillStyle = color;
+        this.ctx.fillText( message, x, y );
+    }
+};
